02601511dab1d181d63a4b3daec03eeaf7163517
bigdeal <bigdeal@smartstudy.co.kr>
2020-06-25 18:00:59 +0900

    update.

--- src/collision.js
+++ src/collision.js
@@ -1,11 +1,11 @@
-export function collision(groupA, groupB, dt) {
+export function collision(objectsA, objectsB, dt) {
     const collisionResults = [];
-    
-    for (let objectA of groupA) {
+
+    for (let objectA of objectsA) {
         const collisionResult = { hit: false, time: Infinity, object: null, position: { x: undefined, y: undefined } };
-        for (let objectB of groupB) {
+        for (let objectB of objectsB) {
             const currentCollisionResult = aabbCollision(objectA, objectB);
-            if (currentCollisionResult.time <= dt && currentCollisionResult.time <= collisionResult.time ) {
+            if (currentCollisionResult.time <= dt && currentCollisionResult.time <= collisionResult.time) {
                 collisionResult = currentCollisionResult;
             }
         }
@@ -18,14 +18,15 @@ export function collision(groupA, groupB, dt) {
 export function aabbCollision(objA, objB) {
     const xAxis = _aabbXAxisCollision(objA, objB);
     const yAxis = _aabbYAxisCollision(objA, objB);
+    const boundary = { min: Infinity, max: -Infinity };
     const result = { hit: false, time: Infinity, object: null, position: { x: undefined, y: undefined } };
-    const timeBoundary = { min: Infinity, max: -Infinity };
+
     if (xAxis.min < yAxis.max && xAxis.max > yAxis.min) {
-        timeBoundary.min = Math.max(xAxis.min, yAxis.min);
-        timeBoundary.max = Math.min(xAxis.max, yAxis.max);
-        if (timeBoundary.max >= 0) {
+        boundary.min = Math.max(xAxis.min, yAxis.min);
+        boundary.max = Math.min(xAxis.max, yAxis.max);
+        if (boundary.max >= 0) {
             result.hit = true;
-            result.time = timeBoundary.min > 0? timeBoundary.min: 0;
+            result.time = boundary.min > 0 ? boundary.min : 0;
             result.object = objB;
             result.position = { x: objA.position.x + objA.vector.x * result.time, y: objA.position.y + objA.vector.y * result.time };
         }
@@ -34,33 +35,29 @@ export function aabbCollision(objA, objB) {
 }
 
 function _aabbYAxisCollision(objA, objB) {
-    const vectorYDiff = objB.vector.y - objA.vector.y;
-    const timestamp = { min: Infinity, max: -Infinity };
-    if (vectorYDiff === 0 && objA.position.y + objA.size.height >= objB.position.y && objA.position.y <= objB.position.y + objB.size.height) {
-        timestamp.min = -Infinity;
-        timestamp.max = Infinity;
-    } else if (vectorYDiff > 0) {
-        timestamp.max = ((objA.position.y + objA.size.height) - objB.position.y) / vectorYDiff;
-        timestamp.min = (objA.position.y - (objB.position.y + objB.size.height)) / vectorYDiff;
-    } else if (vectorYDiff < 0) {
-        timestamp.max = (objA.position.y - (objB.position.y + objB.size.height)) / vectorYDiff;
-        timestamp.min = ((objA.position.y + objA.size.height) - objB.position.y) / vectorYDiff;
+    const VECTOR_DIFFERENCE = objB.vector.y - objA.vector.y;
+    const [BOTTOM_OF_OBJECT_A, TOP_OF_OBJECT_A] = [objA.position.y, objA.position.y + objA.size.height];
+    const [BOTTOM_OF_OBJECT_B, TOP_OF_OBJECT_B] = [objB.position.y, objB.position.y + objB.size.height];
+
+    if (VECTOR_DIFFERENCE === 0 && TOP_OF_OBJECT_A >= BOTTOM_OF_OBJECT_B && BOTTOM_OF_OBJECT_A <= TOP_OF_OBJECT_B) {
+        return { min: -Infinity, max: Infinity };
+    } else if (VECTOR_DIFFERENCE > 0) {
+        return { min: (BOTTOM_OF_OBJECT_A - (TOP_OF_OBJECT_B)) / VECTOR_DIFFERENCE, max: (TOP_OF_OBJECT_A - BOTTOM_OF_OBJECT_B) / VECTOR_DIFFERENCE };
+    } else if (VECTOR_DIFFERENCE < 0) {
+        return { min: (TOP_OF_OBJECT_A - BOTTOM_OF_OBJECT_B) / VECTOR_DIFFERENCE, max: (BOTTOM_OF_OBJECT_A - (TOP_OF_OBJECT_B)) / VECTOR_DIFFERENCE };
     }
-    return timestamp;
 }
 
 function _aabbXAxisCollision(objA, objB) {
-    const vectorXDiff = objB.vector.x - objA.vector.x;
-    const timestamp = { min: Infinity, max: -Infinity };
-    if (vectorXDiff === 0 && objA.position.x + objA.size.width >= objB.position.x && objA.position.x <= objB.position.x + objB.size.width) {
-        timestamp.min = -Infinity;
-        timestamp.max = Infinity;
-    } else if (vectorXDiff > 0) {
-        timestamp.max = ((objA.position.x + objA.size.width) - objB.position.x) / vectorXDiff;
-        timestamp.min = (objA.position.x - (objB.position.x + objB.size.width)) / vectorXDiff;
-    } else if (vectorXDiff < 0) {
-        timestamp.max = (objA.position.x - (objB.position.x + objB.size.width)) / vectorXDiff;
-        timestamp.min = ((objA.position.x + objA.size.width) - objB.position.x) / vectorXDiff;
+    const VECTOR_DIFFERENCE = objB.vector.x - objA.vector.x;
+    const [LEFT_OF_OBJECT_A, RIGHT_OF_OBJECT_A] = [objA.position.x, objA.position.x + objA.size.width];
+    const [LEFT_OF_OBJECT_B, RIGHT_OF_OBJECT_B] = [objB.position.x, objB.position.x + objB.size.width];
+
+    if (VECTOR_DIFFERENCE === 0 && RIGHT_OF_OBJECT_A >= LEFT_OF_OBJECT_B && LEFT_OF_OBJECT_A <= RIGHT_OF_OBJECT_B) {
+        return { min: -Infinity, max: Infinity };
+    } else if (VECTOR_DIFFERENCE > 0) {
+        return { min: (LEFT_OF_OBJECT_A - RIGHT_OF_OBJECT_B) / VECTOR_DIFFERENCE, max: (RIGHT_OF_OBJECT_A - LEFT_OF_OBJECT_B) / VECTOR_DIFFERENCE };
+    } else if (VECTOR_DIFFERENCE < 0) {
+        return { min: (RIGHT_OF_OBJECT_A - LEFT_OF_OBJECT_B) / VECTOR_DIFFERENCE, max: (LEFT_OF_OBJECT_A - RIGHT_OF_OBJECT_B) / VECTOR_DIFFERENCE };
     }
-    return timestamp;
 }
--- src/line.js
+++ src/line.js
@@ -1,6 +1,73 @@
-function mergeLines(lines) {
+function getLightingPolygon(position, lines, length) {
+    const epsilon = 0.01 * Math.PI / 180;
+    const polygon = [];
+
+    for (const line of lines) {
+        const boundary = { begin: Math.atan2(line.sy - position.y, line.sx - position.x), end: Math.atan2(line.ey - position.y, line.ex - position.x) };
+        const radians = [boundary.begin - epsilon, boundary.begin + epsilon, boundary.end - epsilon, boundary.end + epsilon];
+        const lightLines = [
+            { sx: position.x, sy: position.y, ex: position.x + Math.cos(radians[0]) * length, ey: position.y + Math.sin(radians[0]) * length },
+            { sx: position.x, sy: position.y, ex: position.x + Math.cos(radians[1]) * length, ey: position.y + Math.sin(radians[1]) * length },
+            { sx: position.x, sy: position.y, ex: position.x + Math.cos(radians[2]) * length, ey: position.y + Math.sin(radians[2]) * length },
+            { sx: position.x, sy: position.y, ex: position.x + Math.cos(radians[3]) * length, ey: position.y + Math.sin(radians[3]) * length }
+        ];
+
+        for (const lineA of lightLines) {
+            const target = { distance: Infinity, position: undefined };
+
+            for (const lineB of lines) {
+                const result = lineIntersection(lineA, lineB);
+
+                if (result.hit) {
+                    const distance = Math.sqrt((result.position.x - position.x) ** 2 + (result.position.y - position.y) ** 2);
+                    const isNearest = target.distance > distance;
+                    target.position = isNearest ? result.position : target.position;
+                    target.distance = isNearest ? distance : target.distance;
+                }
+            }
+
+            if (target.distance !== Infinity) {
+                polygon.push(target.position);
+            }
+        }
+    }
+
+    return polygon;
+}
+
+function getLineMaxLength(lines) {
+    const boundary = { min: { x: Infinity, y: Infinity }, max: { x: -Infinity, y: -Infinity } };
+    for (const line of lines) {
+        boundary.min.x = boundary.min.x > line.sx ? line.sx : boundary.min.x;
+        boundary.min.x = boundary.min.x > line.ex ? line.ex : boundary.min.x;
+
+        boundary.min.y = boundary.min.y > line.sy ? line.sy : boundary.min.y;
+        boundary.min.y = boundary.min.y > line.ey ? line.ey : boundary.min.y;
+
+        boundary.max.x = boundary.max.x < line.sx ? line.sx : boundary.max.x;
+        boundary.max.x = boundary.max.x < line.ex ? line.ex : boundary.max.x;
+
+        boundary.max.y = boundary.max.y < line.sy ? line.sy : boundary.max.y;
+        boundary.max.y = boundary.max.y < line.ey ? line.ey : boundary.max.y;
+    }
+
+    return Math.sqrt((boundary.max.x - boundary.min.x) ** 2 + (boundary.max.y - boundary.min.y) ** 2);
+}
+
+function lineIntersection(lineA, lineB) {
+    const BASE = (lineB.ey - lineB.sy) * (lineA.ex - lineA.sx) - (lineB.ex - lineB.sx) * (lineA.ey - lineA.sy);
+    if (BASE === 0) return { hit: false, position: { x: undefined, y: undefined } };
+
+    const TIME_A = ((lineB.ex - lineB.sx) * (lineA.sy - lineB.sy) - (lineB.ey - lineB.sy) * (lineA.sx - lineB.sx)) / BASE;
+    const TIME_B = ((lineA.ex - lineA.sx) * (lineA.sy - lineB.sy) - (lineA.ey - lineA.sy) * (lineA.sx - lineB.sx)) / BASE;
+    if (TIME_A < 0 || TIME_A > 1 || TIME_B < 0 || TIME_B > 1) return { hit: false, position: { x: undefined, y: undefined } };
+
+    return { hit: true, position: { x: lineA.sx + TIME_A * (lineA.ex - lineA.sx), y: lineA.sy + TIME_A * (lineA.ey - lineA.sy) } };
+}
+
+function merge(lines) {
     let mergedLines = [];
-    const axisLines= {
+    const axisLines = {
         x: [...getAxisLines(lines, { x: 1, y: 0 }), ...getAxisLines(lines, { x: -1, y: 0 })],
         y: [...getAxisLines(lines, { x: 0, y: 1 }), ...getAxisLines(lines, { x: 0, y: -1 })],
         uxy: [...getAxisLines(lines, { x: -1, y: -1 }), ...getAxisLines(lines, { x: 1, y: 1 })],
@@ -8,14 +75,14 @@ function mergeLines(lines) {
     };
 
     for (let key in axisLines) {
-        mergedLines = [...mergedLines, ...merge(axisLines[key])];
+        mergedLines = [...mergedLines, ...lineMerge(axisLines[key])];
     }
     return mergedLines.map((line) => {
         return { sx: line.sx, ex: line.ex, sy: line.sy, ey: line.ey };
     });
 }
 
-function merge(lines) {
+function lineMerge(lines) {
     const mergedLines = [];
 
     for (let lineA of lines) {
@@ -44,83 +111,40 @@ function merge(lines) {
 }
 
 function getAxisLines(lines, vector) {
-  return lines.reduce((p, c) => {
-    const standard = Math.abs(c.ex - c.sx) > Math.abs(c.ey - c.sy)? Math.abs(c.ex - c.sx): Math.abs(c.ey - c.sy);
-    const x = (c.ex - c.sx) / standard;
-    const y = (c.ey - c.sy) / standard;
+    return lines.reduce((p, c) => {
+        const standard = Math.abs(c.ex - c.sx) > Math.abs(c.ey - c.sy) ? Math.abs(c.ex - c.sx) : Math.abs(c.ey - c.sy);
+        const x = (c.ex - c.sx) / standard;
+        const y = (c.ey - c.sy) / standard;
 
-    if (x === vector.x && y === vector.y) {
-        p.push({sx:c.sx,ex:c.ex,sy:c.sy,ey:c.ey});
-    }
-    return p;
-  }, []);
+        if (x === vector.x && y === vector.y) {
+            p.push({ sx: c.sx, ex: c.ex, sy: c.sy, ey: c.ey });
+        }
+        return p;
+    }, []);
 }
 
 function main() {
-    const lines = [
-        {
-            sx: 0,
-            ex: 1,
-            sy: 0,
-            ey: 0
-        },{
-            sx: 1,
-            ex: 2,
-            sy: 0,
-            ey: 0
-        },{
-            sx: 0,
-            ex: -1,
-            sy: 0,
-            ey: 0
-        },{
-            sx: -1,
-            ex: -2,
-            sy: 0,
-            ey: 0
-        },{
-            sx: 0,
-            ex: 0,
-            sy: 0,
-            ey: 1
-        },{
-            sx: 0,
-            ex: 0,
-            sy: 1,
-            ey: 2
-        },{
-            sx: 0,
-            ex: 0,
-            sy: 0,
-            ey: -1
-        },{
-            sx: 0,
-            ex: 0,
-            sy: -1,
-            ey: -2
-        },{
-            sx: 0,
-            ex: -1,
-            sy: 0,
-            ey: -1
-        },{
-            sx: -1,
-            ex: -2,
-            sy: -1,
-            ey: -2
-        },{
-            sx: 0,
-            ex: 1,
-            sy: 0,
-            ey: -1
-        },{
-            sx: 2,
-            ex: 1,
-            sy: -2,
-            ey: -1
-        }
-    ];
-    mergeLines(lines);
+    const lines = [];
+
+    for (let i = 0; i < 100; i++) {
+        lines.push({
+            sx: Math.random() * 100 - Math.random() * 100,
+            sy: Math.random() * 100 - Math.random() * 100,
+            ex: Math.random() * 100 - Math.random() * 100,
+            ey: Math.random() * 100 - Math.random() * 100
+        });
+    }
+
+    // const mergedLines = merge(lines);
+    const light = { x: 0, y: 0 };
+
+    const begin = Date.now();
+    const length = getLineMaxLength(lines);
+    for (let i =0; i < 20; i++) {
+        const polygon = getLightingPolygon(light, lines, length);
+    }
+    const end = Date.now();
+    console.log(`${1000/(end - begin)} ups`);
 }
 
 main();
